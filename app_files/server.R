691e92889e2f9e87f227963730d69bfa5b632ede
Robert Smith <rsmith@bresmed.com>
2022-04-14 23:13:57 +0100

    add a shiny app to the mix, why not

--- app_files/server.R
+++ app_files/server.R
@@ -1,12 +1,91 @@
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  list_results <- eventReactive(input$runModel, {
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # convert inputs into a single data-frame to be passed to the API call
+    df_input <- data.frame(
+      parameter = c("p_HS1", "u_S1"),
+      distribution = c(input$p_HS1_dist, input$u_S1_dist),
+      v1 = c(input$p_HS1_v1, input$u_S1_v1),
+      v2 = c(input$p_HS1_v2, input$u_S1_v2)
+    )
+    
+    # show modal saying sending to API
+    show_modal_gif(text = "Interacting with client API",
+                   modal_size = "l",
+                   width = "200px", 
+                   height = "300px",
+                   src = "bean.gif"
+                  )
+    
+    # run the model using the connect server API
+    results <- httr::content(
+      httr::POST(
+        # the Server URL can also be kept confidential, but will leave here for now 
+        url = "https://connect.bresmed.com",
+        # path for the API within the server URL
+        path = "rhta2022/runDARTHmodel",
+        # code is passed to the client API from GitHub.
+        query = list(model_functions = "https://raw.githubusercontent.com/BresMed/plumberHE/main/R/darth_funcs.R"),
+        # set of parameters to be changed ... we are allowed to change these but not some others...
+        body = jsonlite::toJSON(df_input),
+        # we include a key here to access the API ... like a password protection
+        config = httr::add_headers(Authorization = paste0("Key ", 
+                                                          Sys.getenv("CONNECT_KEY")))
+      )
+    )
+    # show modal saying finished getting data from API
+    remove_modal_gif()
+    
+    # rename the costs columns
+    results_C <- results[,1:3]
+    # same for qalys
+    results_Q <- results[,4:6]
+    # name all the columns the same
+    colnames(results_C) <- colnames(results_Q) <- c("A", "B", "C")
+    
+    # create ceac based on brandtools package from lumanity...
+    temp_cols <- c("#D8D2BF", "#001B2B", "#007B84")
+    names(temp_cols) <- c("A", "B", "C")
+    
+    list("results_C" = results_C, 
+         "results_Q" = results_Q,
+         "temp_cols" = temp_cols)
+    
   })
+  
+    output$CEPlane <- renderPlot({
+      
+      
+      # create the CEP
+      makeCEPlane(total_costs = list_results()$results_C,
+                  total_qalys = list_results()$results_Q,
+                  treatment = "B",
+                  comparitor = "A",
+                  thresh = 30000,
+                  show_ellipse = T,
+                  colors = list_results()$temp_cols)
+
+    })
+    
+    
+    output$CEAC <- renderPlot({
+      
+      makeCEAC(total_costs = list_results()$results_C,
+               total_qalys = list_results()$results_Q,
+               treatment = c("A", "B", "C"),
+               lambda_min = 0,
+               lambda_max = 100000,
+               col = list_results()$temp_cols)
+      
+    })
+    
+    output$ResultsTab_C <- renderTable({
+      head(list_results()$results_C)
+      })
+    
+    output$ResultsTab_Q <- renderTable({
+      head(list_results()$results_Q)
+      })
 }
--- app_files/ui.R
+++ app_files/ui.R
@@ -2,21 +2,90 @@
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Living HTA - Demo Shiny App"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
+    
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      tabsetPanel(
+        tabPanel(title = "State Trans Probs", 
+                 
+                 #=====================================#
+                 # Transition Prob from Healthy to Sick
+                 #=====================================#
+                 
+                 selectInput(inputId = "p_HS1_dist",
+                             label = "p(S1 | H): Distribution",
+                             choices = c("beta", "gamma", "rlnorm", "fixed"),
+                             selected = "beta"),
+                 
+                 numericInput(inputId = "p_HS1_v1",
+                              label = "p(S1 | H): Parameter 1",
+                              min = 0,
+                              max = 200,
+                              value = 30),
+                 
+                 numericInput(inputId = "p_HS1_v2",
+                              label = "p(S1 | H): Parameter 2",
+                              min = 0,
+                              max = 200,
+                              value = 170)
+                 ),
+        
+        tabPanel(title = "Utilities", 
+                 #=====================================#
+                 # Transition Prob from Healthy to Sick
+                 #=====================================#
+                 
+                 selectInput(inputId = "u_S1_dist",
+                             label = "Utility S1: Distribution",
+                             choices = c("beta", "gamma", "rlnorm", "fixed"),
+                             selected = "beta"),
+                 
+                 numericInput(inputId = "u_S1_v1",
+                              label = "Utility S1: Parameter 1",
+                              min = 0,
+                              max = 200,
+                              value = 130),
+                 
+                 numericInput(inputId = "u_S1_v2",
+                              label = "Utility S1: Parameter 2",
+                              min = 0,
+                              max = 200,
+                              value = 45)
+                 )
+      ),
+      
+    
+      
+      
+      
+      # action button to run the model
+      actionButton("runModel", "Run Model"),
+      
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      tabsetPanel(
+        tabPanel(title = "Model Output",
+                 column(width = 6,align="center",
+                        h2("QALYs"),
+                        tableOutput(outputId = "ResultsTab_Q")),
+                 column(width = 6,align="center",
+                        h2("Costs"),
+                        tableOutput(outputId = "ResultsTab_C"))),
+        tabPanel(title = "CE Plane", 
+                 plotOutput(outputId = "CEPlane")),
+        tabPanel(title = "CEAC", 
+                 plotOutput(outputId = "CEAC"))
+      )
+      
     )
-  )
+  ),
+  
+  "Important take-home from this very simple demo app is that the designer of the app
+       does not need to have: the model code, any data, any knowledge of health economics.
+       They just connect numeric inputs to JSON inputs to the model as requested by a health economist"
 )
