42b351c23cb34a50db3db421da2853dcb7382c14
Robert Smith <rsmith@bresmed.com>
2022-04-14 10:33:30 +0100

    schedule cron job.

--- scripts/run_darthAPI.R
+++ scripts/run_darthAPI.R
@@ -1,17 +1,28 @@
 # remove all existing data from the environment.
 rm(list = ls())
 
+library(ggplot2)
+library(darkpeak)
+library(jsonlite)
+library(httr)
+library(devtools)
+
 # run the model using the connect server API
 results <- httr::content(
   httr::POST(
-    url = Sys.getenv("SERVER_URL"),
+    # the Server URL can also be kept confidential, but will leave here for now 
+    url = "https://connect.bresmed.com",
+    # path for the API within the server URL
     path = "rhta2022/runDARTHmodel",
-    query = list(model_functions = "https://gist.githubusercontent.com/RobertASmithBresMed/09308560fffbe79a5e474e7dad54ea3e/raw/dfc518c7474fa577bf68bec2a60e0246fdbd4ec2/DARTHcode.R"),
+    # code is passed to the client API from GitHub.
+    query = list(model_functions = "https://raw.githubusercontent.com/BresMed/plumberHE/main/R/darth_funcs.R"),
+    # set of parameters to be changed ... we are allowed to change these but not some others...
     body = jsonlite::toJSON(data.frame(parameter = c("p_HS1","p_S1H"),
                                        distribution = c("beta","beta"),
                                        v1 = c(25, 50),
                                        v2 = c(150, 100))),
-    httr::add_headers(Authorization = paste0("Key ", 
+    # we include a key here to access the API ... like a password protection
+    config = httr::add_headers(Authorization = paste0("Key ", 
                                              Sys.getenv("CONNECT_KEY")))
   )
 )
@@ -20,8 +31,8 @@ results <- httr::content(
 write.csv(x = results,
           file = "outputs/darth_model_results.csv")
 
-# render the markdown document from the report folder, passing the results from above
-# to the report.
-rmarkdown::render(input = "report/darthreport.Rmd",
-                  params = list("df_results" = results),
-                  output_dir = "outputs")
+# render the markdown document from the report folder, 
+# passing the results dataframe to the report.
+#rmarkdown::render(input = "report/darthreport.Rmd",
+#                  params = list("df_results" = results),
+#                  output_dir = "outputs")
